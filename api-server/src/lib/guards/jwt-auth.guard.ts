4d296dfff8a84358a22dac7557c45b4e3b181e88
hamabehayato <be_dash1122@icloud.com>
2023-12-27 08:12:38 +0900

    add: authGuard のテストコード追加

--- api-server/src/lib/guards/jwt-auth.guard.ts
+++ api-server/src/lib/guards/jwt-auth.guard.ts
@@ -8,26 +8,28 @@ export const authGuard = (req: Request, res: Response, next: NextFunction) => {
     if (!authHeader) {
       return res.status(401).json({
         errorCode: 401,
-        errorMessage: '認証が必要です。',
+        errorMessage: 'ヘッダー authorization がありません',
       });
     }
 
     const token = authHeader.split(' ')[1];
 
     if (!token) {
-      return res.status(401).json({ error: 'Unauthorized: Token missing' });
+      return res.status(401).json({ error: 'アクセストークンがありません' });
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string) as {
       userId: number;
     };
 
-    // リクエストオブジェクトにuserIdをセット
-    req.body.userId = decoded.userId;
+    if (req?.body) {
+      // リクエストオブジェクトにuserIdをセット
+      req.body.userId = decoded.userId;
+    }
 
     // 次のミドルウェアまたはルートハンドラに制御を渡す
     next();
   } catch (error) {
-    res.status(500).json({ error: 'authGuard' });
+    res.status(500).json({ error: '認証エラー' });
   }
 };
